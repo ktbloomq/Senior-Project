7d79f194e4cc5b20fcc11936c39c8b1326889c74
Kevin Bloomquist <ktb@blooquist.com>
2024-03-07 14:18:34 -0800

    database prep

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express, { RequestHandler, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import { execute } from './services/mysql.connector';
 
 dotenv.config();
 const app: express.Application = express();
 const port = process.env.PORT;
+app.use(cors());
+app.use(helmet());
 
-app.get('/', (_req, _res) => {
-  _res.send("Typescript with express");
-});
+let getUsers:RequestHandler = async function(req: Request, res: Response) {
+  let users = await execute("SELECT * FROM  users",[]);
+  res.status(200).json(users);
+}
+
+app.get('/', getUsers);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,12 @@
+import Post from "./post";
+
 export default function Home() {
   return (
     <div className="container">
       <h1 className="text-center">Travel Journal Feed</h1>
       <hr />
-      <div className="card mb-2">
-        <div className="border-bottom bg-secondary">
-          <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" style={{ objectFit: "contain", width: "100%", maxHeight: "70vh" }} alt="..." />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title d-flex align-items-end">
-            <div className="me-1">null</div>
-            <div className="text-secondary fs-6 fst-italic">by user</div>
-            <div className="ms-auto text-secondary fs-6">📍
-              <span>null</span>
-            </div>
-          </h5>
-          <p className="card-text">null</p>
-          <div className="text-end">
-            <a className="btn btn-outline-primary">
-              <span>0</span> 👍
-            </a>
-          </div>
-        </div>
-      </div>
+      <Post />
+      <Post />
     </div>
   );
 }
