a71003fa63dff2e178d9c71cdabcc9121088dd3c
Kevin Bloomquist <ktb@blooquist.com>
2024-05-18 12:10:31 -0700

    frontend: update titlebar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,31 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/options';
+import Titlebar from './titlebarAuthenticated';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <body>
         <main>
-          <nav className="navbar navbar-expand-sm bg-body-tertiary border-bottom container-fluid">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="/">Home</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>    
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav me-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="/post">Create Post</a>
-                  </li>    
-                </ul>
-                <a href="#" className="btn btn-outline-primary me-2">Login</a>
-                <a href="#" className="btn btn-outline-primary me-2">Log out</a>
-              </div>
-            </div>
-          </nav>
+          <Titlebar isAuthenticated={session!==null} />
           <div className="container">
             {children}
           </div>
---
+++ frontend/src/app/titlebarAuthenticated.tsx
@@ -0,0 +1,24 @@
+export default function Titlebar({isAuthenticated}:{isAuthenticated:boolean}) {
+    return (
+        <nav className="navbar navbar-expand-sm bg-body-tertiary border-bottom container-fluid">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Home</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/post">Create Post</a>
+                        </li>
+                    </ul>
+                    {isAuthenticated ? 
+                        <a href="/api/auth/signout" className="btn btn-outline-primary me-2">Log out</a>
+                        :
+                        <a href="/api/auth/signin" className="btn btn-outline-primary me-2">Login</a>
+                    }
+                </div>
+            </div>
+        </nav>
+    )
+}
